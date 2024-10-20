73fcc5509c9e1a939b2f17a90e76b7ee589982b3
aliagasiyev <agasiyevali2@gmail.com>
2024-10-20 15:41:37 +0400

    first commit

---
+++ src/main/java/az/edu/turing/msunitech/MsUnitechApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.msunitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUnitechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsUnitechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/edu/turing/msunitech/MsUnitechApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.msunitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUnitechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
